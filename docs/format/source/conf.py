f529e92f6be3d8ca567ba1d043e2406740f78f14
Ryan Ly <rly@lbl.gov>
2020-01-14 17:29:25 -0800

    Fix copyright

--- docs/format/source/conf.py
+++ docs/format/source/conf.py
@@ -75,8 +75,8 @@ master_doc = 'source/index'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Hierarchical Data Modeling Framework'
-copyright = u'2017-2020, Hierarchical Data Modeling Framework'
+project = u'Neurodata Without Borders'
+copyright = u'2017-2020, Neurodata Without Borders'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
