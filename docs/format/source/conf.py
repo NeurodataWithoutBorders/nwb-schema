ea75c177c98397fd1b04a41f2a50ba3bf33f7be3
Ryan Ly <rly@lbl.gov>
2020-03-02 19:51:08 -0800

    Update version number to 2.2.2

--- docs/format/source/conf.py
+++ docs/format/source/conf.py
@@ -83,7 +83,7 @@ copyright = u'2017-2020, Neurodata Without Borders'
 # built documents.
 #
 # The short X.Y version.
-version = 'v2.2.1'
+version = 'v2.2.2'
 # The full version, including alpha/beta/rc tags.
 release = version
 
